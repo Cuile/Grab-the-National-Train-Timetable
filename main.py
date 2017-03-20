a67f1a6bace164e009187880658affa5a2e314b8
cuile <cuile@feinno.com>
2017-03-20 21:29:58 +0800

    使用grab.train_schedule类，获取列车时刻表 添加新的测试用例

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ if __name__ == '__main__':
     pass
     # print(grab.grab_station_name())
     # print(grab.grab_train_list())
-    print(grab.grab_train_schedule('2017-03-19'))
-    # print(shortest_path.get_edges('2017-03-19'))
+    ts = grab.train_schedule()
+    print(ts.get_train_schedule(d='2017-03-19', max_workers=10))
+    print(shortest_path.get_edges('2017-03-19'))
     # ------------------------------------------------------------------------
     # with open('edges.json', 'r', encoding='utf-8') as e:
     #     edges = json.load(e)
-
-    # print(shortest_path.get_shortest_path(edges, '北京', '成都南'))
-    # print(shortest_path.get_shortest_path(edges, '成都南', '广元'))
+    #
+    # print(shortest_path.get_shortest_path(edges, '广州南', '北京西'))
+    # print(shortest_path.get_shortest_path(edges, '广州东', '北京西'))
+    # print(shortest_path.get_shortest_path(edges, '广州', '北京西'))
+    # print(shortest_path.get_shortest_path(edges, '广州西', '北京西'))
+    # print(shortest_path.get_shortest_path(edges, '广州北', '北京西'))
