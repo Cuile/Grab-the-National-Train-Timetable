745939c8e86e5f2aa230eaa896b6e7134603ef2e
cuile <cuile@feinno.com>
2017-03-20 21:26:50 +0800

    添加grab()方法的超时异常处理 增加train_schedule类，替换grab_callback(), grab_train_schedule()方法

--- grab.py
+++ grab.py
@@ -25,7 +25,11 @@ def grab(url):
         'Referer': 'https://kyfw.12306.cn/otn/leftTicket/init',
         'Accept-Encoding': 'gzip, deflate, sdch, br',
         'Accept-Language': 'zh-CN,zh;q=0.8'}
-    return requests.get(url, headers=headers, verify=False, timeout=5).text
+    try:
+        return requests.get(url, headers=headers, verify=False, timeout=15).text
+    except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
+        print(url)
+        return '["timeout"]'
 
 
 # ------------------------------------------------------------------------
@@ -78,78 +82,118 @@ def grab_train_list():
 
 
 # ------------------------------------------------------------------------
+class train_schedule():
+    def get_train_schedule(self, d, max_workers):
+        # self.grab_train_schedule(d, max_workers)
+        self.parse_train_schedule()
 
+    def parse_train_schedule(self):
+        with open('train_schedule.json', 'r', encoding='utf-8') as ts:
+            train_schedule = json.load(ts)
 
-def grab_callback(url):
-    ts = json.loads(grab(url))
-    try:
-        train = ts['data']
-        ts.clear()
-        ts['train'] = train
-        ts['train']['schedule'] = ts['train']['data']
-        del ts['train']['data']
-        ts['train']['start_station_name'] = ts['train']['schedule'][0]['start_station_name']
-        del ts['train']['schedule'][0]['start_station_name']
-        ts['train']['end_station_name'] = ts['train']['schedule'][0]['end_station_name']
-        del ts['train']['schedule'][0]['end_station_name']
-        ts['train']['station_train_code'] = ts['train']['schedule'][0]['station_train_code']
-        del ts['train']['schedule'][0]['station_train_code']
-        ts['train']['train_class_name'] = ts['train']['schedule'][0]['train_class_name']
-        del ts['train']['schedule'][0]['train_class_name']
-        ts['train']['service_type'] = ts['train']['schedule'][0]['service_type']
-        del ts['train']['schedule'][0]['service_type']
-        print(ts)
-        return [True, ts]
-    except IndexError as err:
-        return [False, ts]
+        get_train_schedule = []
+        get_train_schedule_err = []
 
+        for ts in train_schedule:
+            try:
+                train = ts['data']
+                ts.clear()
+                ts['train'] = train
 
-def grab_train_schedule(d):
-    common.timing_starts()
+                ts['train']['schedule'] = ts['train']['data']
+                del ts['train']['data']
 
-    with open('station_name.json', 'r', encoding='utf-8') as sn, open('train_list.json', 'r', encoding='utf-8') as tl:
-        station_name = json.load(sn)
-        train_list = json.load(tl)
+                ts['train']['start_station_name'] = ts['train']['schedule'][0][
+                    'start_station_name']
+                del ts['train']['schedule'][0]['start_station_name']
 
-    year, month, day = d.split('-')
-    start_time = date(int(year), int(month), int(day))
-    dates = [
-        start_time - timedelta(days=3),
-        start_time - timedelta(days=2),
-        start_time - timedelta(days=1),
-        start_time,
-        start_time + timedelta(days=1),
-        start_time + timedelta(days=2),
-        start_time + timedelta(days=3)
-    ]
-    urls = []
-    for d in dates:
-        for i in train_list:
-            train_no = i['train_no']
-            for j in station_name:
-                if i['from_station'] == j['station_name']:
-                    from_station_telecode = j['telecode']
-                if i['to_station'] == j['station_name']:
-                    to_station_telecode = j['telecode']
-            urls.append('https://kyfw.12306.cn/otn/czxx/queryByTrainNo?train_no=%s&from_station_telecode=%s&to_station_telecode=%s&depart_date=%s' % (
-                train_no,
-                from_station_telecode,
-                to_station_telecode,
-                d.isoformat()
-            ))
+                ts['train']['end_station_name'] = ts['train']['schedule'][0]['end_station_name']
+                del ts['train']['schedule'][0]['end_station_name']
 
-    train_schedule = []
-    ts_err = []
-    with ThreadPoolExecutor(max_workers=5) as pool:
-        for i in pool.map(grab_callback, urls):
-            if i[0]:
-                train_schedule.append(i[1])
-            else:
-                ts_err.append(i[1])
+                ts['train']['station_train_code'] = ts['train']['schedule'][0][
+                    'station_train_code']
+                del ts['train']['schedule'][0]['station_train_code']
 
-    with open('train_schedule.json', 'w', encoding='utf-8') as fp:
-        json.dump(train_schedule, fp, ensure_ascii=False, sort_keys=True, indent=2)
+                ts['train']['train_class_name'] = ts['train']['schedule'][0]['train_class_name']
+                del ts['train']['schedule'][0]['train_class_name']
 
-    common.timing_ends('抓取列车时刻表')
+                ts['train']['service_type'] = ts['train']['schedule'][0]['service_type']
+                del ts['train']['schedule'][0]['service_type']
 
-    return True
+                ts['train']['s'] = sorted(ts['train']['schedule'], key=lambda x: x['station_no'])
+                ts['train']['schedule'] = {}
+                schedule_index = 0
+                for td in ts['train']['s']:
+                    ts['train']['schedule'].update({schedule_index: td})
+                    schedule_index += 1
+                del ts['train']['s']
+
+                get_train_schedule.append(ts)
+
+            except (IndexError, TypeError, json.decoder.JSONDecodeError):
+                pass
+
+        with open('get_train_schedule.json', 'w', encoding='utf-8') as fp:
+            json.dump(get_train_schedule, fp, ensure_ascii=False, sort_keys=True, indent=2)
+
+    def grab_train_schedule_callback(self, url):
+        try:
+            ts = json.loads(grab(url))
+            # print(ts)
+            return [True, ts]
+        except json.decoder.JSONDecodeError:
+            # print(ts)
+            return [False, ts]
+
+    def grab_train_schedule(self, d, max_workers):
+        common.timing_starts()
+
+        with open('station_name.json', 'r', encoding='utf-8') as sn:
+            station_name = json.load(sn)
+        with open('train_list.json', 'r', encoding='utf-8') as tl:
+            train_list = json.load(tl)
+
+        year, month, day = d.split('-')
+        start_time = date(int(year), int(month), int(day))
+        # dates = [
+        #     start_time - timedelta(days=3),
+        #     start_time - timedelta(days=2),
+        #     start_time - timedelta(days=1),
+        #     start_time,
+        #     start_time + timedelta(days=1),
+        #     start_time + timedelta(days=2),
+        #     start_time + timedelta(days=3)
+        # ]
+        dates = [start_time]
+        urls = []
+        for d in dates:
+            for i in train_list:
+                train_no = i['train_no']
+                for j in station_name:
+                    if i['from_station'] == j['station_name']:
+                        from_station_telecode = j['telecode']
+                    if i['to_station'] == j['station_name']:
+                        to_station_telecode = j['telecode']
+                urls.append(
+                    'https://kyfw.12306.cn/otn/czxx/queryByTrainNo?train_no=%s&from_station_telecode=%s&to_station_telecode=%s&depart_date=%s' % (
+                        train_no,
+                        from_station_telecode,
+                        to_station_telecode,
+                        d.isoformat()
+                    ))
+
+        train_schedule = []
+        train_schedule_err = []
+        with ThreadPoolExecutor(max_workers=max_workers) as pool:
+            for i in pool.map(self.grab_train_schedule_callback, urls):
+                if i[0]:
+                    train_schedule.append(i[1])
+                else:
+                    train_schedule_err.append(i[1])
+
+        with open('train_schedule.json', 'w', encoding='utf-8') as fp:
+            json.dump(train_schedule, fp, ensure_ascii=False, sort_keys=True, indent=2)
+
+        common.timing_ends('抓取列车时刻表')
+
+        return True
